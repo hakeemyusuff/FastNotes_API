4cafc70f05c9ed65fd98e6a6a52ab8343e15fd21
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-08-11 08:29:21 +0100

    feat: add delete note, view a single note, and search note

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ from datetime import datetime
 app = FastAPI()
 
 
-id = 0
-notes: dict[int, dict[str, str]] = {}
-
 class Note(BaseModel):
     title: str
     body: str
@@ -15,13 +12,42 @@ class Note(BaseModel):
     edited_at: datetime = datetime.now()
 
 
+id = 0
+notes: dict[int, dict[str, str]] = {}
+
 
 @app.get("/api/notes")
-async def get_notes():
+async def get_notes(search: str | None = None):
     """This endpoints returns all the notes"""
     if not notes:
         return {"message": "You currently don't have any notes.."}
+    if search is not None and search != "":
+        normalised_search = search.strip().lower()
+        search_result: dict[int, dict[str, str]] = {}
+        for key, note_dict in notes.items():
+            id = key
+            title = note_dict["title"].lower()
+            body = note_dict["body"].lower()
+            if normalised_search in title or normalised_search in body:
+                note = notes[id]
+                search_result.update({id : note})
+                continue
+        return search_result
+
     return notes
+
+@app.get("/api/notes/{note_id}")
+async def get_note(note_id: int):
+    note = notes.get(note_id, "This note have been moved or deleted.")
+    return note
+
+@app.delete("/api/notes/{note_id}")
+async def delete_note(note_id: int):
+    if notes.get(note_id):
+        notes.pop(note_id)
+        return {"message": f"note with id {note_id} deleted successfully."}
+    else:
+        return {"message": "This note doesn't exist."}
 
 @app.post("/api/notes/")
 async def add_note(note: Note):
@@ -29,6 +55,6 @@ async def add_note(note: Note):
     if note:
         note_dict = Note.model_dump(note)
         note_dict.update({"id": id})
-        notes.update({id :note_dict})
+        notes.update({id : note_dict})
         id = id + 1
     return {"message": "Note added successfully.", "note": note_dict}
